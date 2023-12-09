bc4dd7aadf0af710e4ba5cdfaba49ee5cf5c1f18
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-09 17:09:59 -0300

    feat: Created a home page specifically for searching profiles.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState("");
+
+  const handleSubmit = () => {
+    navigate(`/${user}`);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter") {
+      handleSubmit();
+    }
+  };
+
   return (
     <section>
-      <img src="" alt="" />
-      <h1></h1>
-      <h2></h2>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      <input
+        type="text"
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={() => handleSubmit()}></button>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
