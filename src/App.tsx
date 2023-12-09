4f67d3d35e61cf539ea30eca32da5517a2d4196d
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-09 11:45:27 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Home from './pages/Home.tsx'
+import './styles/Index.css'
+function App() {
+
+  return (
+    <Home/>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <section>
+      <img src="" alt="" />
+      <h1></h1>
+      <h2></h2>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+  )
+}
+
+export default Home
