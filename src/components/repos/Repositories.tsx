cd805b9234dcac95d449373c31ca86960b428c7b
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-13 11:51:40 -0300

    feat: Added a parameter '?sort=pushed' to the search to get the user repositories last update.

--- src/components/repos/Repositories.tsx
+++ src/components/repos/Repositories.tsx
@@ -14,13 +14,14 @@ type Data = {
   language: string;
   html_url: string;
   stargazers_count: number;
+  archived: boolean;
 };
 
 const Repositories: React.FC = () => {
   const navigate = useNavigate();
   const params = useParams();
   const [maxCards, setMaxCards] = useState<number>(9);
-  const url = `https://api.github.com/users/${params.user}/repos`;
+  const url: string = `https://api.github.com/users/${params.user}/repos?sort=pushed`;
   const { data, error, loading } = useFetchData<Data[]>(url);
 
   if (error) navigate("/");
@@ -35,33 +36,38 @@ const Repositories: React.FC = () => {
         <p>Loading...</p>
       ) : (
         <Container>
-          {data.slice(0, maxCards).map((repo) => (
-            <div key={repo.id} className="card">
-              <div className="card-header">
-                <h1>{repo.name}</h1>
-                <div className="card-buttons">
-                  {repo.homepage && (
-                    <button
-                      onClick={() => window.open(repo.homepage, "_blank")}
-                    >
-                      <LaunchIcon />
-                    </button>
-                  )}
-                  <button onClick={() => window.open(repo.html_url, "_blank")}>
-                    <GitHubIcon />
-                  </button>
+          {data.slice(0, maxCards).map(
+            (repo) =>
+              !repo.archived && (
+                <div key={repo.id} className="card">
+                  <div className="card-header">
+                    <h1>{repo.name}</h1>
+                    <div className="card-buttons">
+                      {repo.homepage && (
+                        <button
+                          onClick={() => window.open(repo.homepage, "_blank")}
+                        >
+                          <LaunchIcon />
+                        </button>
+                      )}
+                      <button
+                        onClick={() => window.open(repo.html_url, "_blank")}
+                      >
+                        <GitHubIcon />
+                      </button>
+                    </div>
+                  </div>
+                  <p>{repo.description}</p>
+                  <div className="card-footer">
+                    <span>{repo.language}</span>
+                    <span>
+                      {repo.stargazers_count}
+                      <StarIcon style={{ fontSize: "14px" }} />
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <p>{repo.description}</p>
-              <div className="card-footer">
-                <span>{repo.language}</span>
-                <span>
-                  {repo.stargazers_count}
-                  <StarIcon style={{ fontSize: "14px" }} />
-                </span>
-              </div>
-            </div>
-          ))}
+              )
+          )}
         </Container>
       )}
       {maxCards < data.length ? (
